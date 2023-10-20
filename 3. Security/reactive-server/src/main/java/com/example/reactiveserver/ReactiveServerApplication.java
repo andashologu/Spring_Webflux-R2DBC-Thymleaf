308a54c497c095a0a55a74adca026ca775854752
Anda Shologu <SHOLOGUA@cput.ac.za>
2023-10-20 12:10:47 +0200

    from W

--- 3. Security/reactive-server/src/main/java/com/example/reactiveserver/ReactiveServerApplication.java
+++ 3. Security/reactive-server/src/main/java/com/example/reactiveserver/ReactiveServerApplication.java
@@ -2,8 +2,10 @@ package com.example.reactiveserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class ReactiveServerApplication {
 
 	public static void main(String[] args) {
